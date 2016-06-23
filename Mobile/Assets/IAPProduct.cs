7e0de8d486bf645b9f593c6861c7d92ed8a2b399
Shane Martin <ghoulminy@live.com>
2016-06-23 10:53:58 +1000

    IAP Stuff

---
+++ Mobile/Assets/IAPProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class IAPProduct {
+
+	public string productStringID;
+	public ProductType productType;
+	public UnityEvent FuctionToCallOnPurchaseComplete;
+}
